453c29cfdda4849bb6fd0b39a2ee167b4ae9b54a
mitali shah <mitalishaleenshah@gmail.com>
2020-07-16 14:40:06 -0700

    passing user's selection to the play function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 "use strict";
 
+let play = function (selected) {
+  //play the game
+};
+
 // Listen for click events
 document.addEventListener(
   "click",
-  function () {
+  function (event) {
     let selected = event.target.getAttribute("data-rps");
     console.log(selected);
     if (!selected) return;
+    play(selected);
   },
   false
 );
