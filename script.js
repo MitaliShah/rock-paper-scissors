a12e67f16c665de8c035b6dce6ece46687bde420
mitali shah <mitalishaleenshah@gmail.com>
2020-07-16 14:50:26 -0700

    randomly pick one item from array for opponent and store that value in play function with opponent variable

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 "use strict";
 
+// Variables
+var options = ["rock", "paper", "scissors"];
+
 let play = function (selected) {
-  //play the game
+  let opponent = getOpponent();
+};
+
+// Randomly shuffle an array
+
+let shuffle = function (array) {
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+// Get a random pick for the opponent
+
+let getOpponent = function () {
+  return shuffle(options.slice())[0];
 };
 
 // Listen for click events
